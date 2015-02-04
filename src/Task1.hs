68fd60f7eab69067d1c908be3ed1f60145091f8f
EvgenyZamyatin <jeka_zamyatin@mail.ru>
2015-02-04 20:26:15 +0300

    ololo

--- src/Task1.hs
+++ src/Task1.hs
@@ -15,7 +15,7 @@ toString :: [(String, Annotation)] -> String
 toString [] = ""
 toString ((a,b):xs) = a ++ " " ++ (show b) ++ "\n" ++ toString xs
 
-main = createDirectory "out" >> fmap lines (readFile ("tests/HW1/test_list.txt")) >>= f  
+main = createDirectory "out" >> fmap lines (readFile ("../tests/HW1/test_list.txt")) >>= f  
 	where
 		f :: [String] -> IO ()
 		f [s] = calc s >> putStrLn ("complete " ++ s)
@@ -23,7 +23,7 @@ main = createDirectory "out" >> fmap lines (readFile ("tests/HW1/test_list.txt")
 
 calc :: String -> IO ()
 calc s = 
-	fmap f (readFile ( "tests/HW1/"++ s ++ ".in")) >>= writeFile ("out/" ++ s ++ ".out") 
+	fmap f (readFile ( "../tests/HW1/"++ s ++ ".in")) >>= writeFile ("out/" ++ s ++ ".out") 
 		where
 			f str = toString (zip list (verify A.axiomList [] (map parse list)))
 				where list = lines str
